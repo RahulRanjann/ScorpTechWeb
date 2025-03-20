7075df7545d3034f97e1b36273babedc31240761
rahulranjann <rahul1913111054@gmail.com>
2025-03-20 10:56:17 +0530

    hero section fixed

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -30,8 +30,8 @@ export function HeroSection() {
       opacity: number;
 
       constructor() {
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
+        this.x = Math.random() * (canvas?.width || window.innerWidth);
+        this.y = Math.random() * (canvas?.height || window.innerHeight);
         this.size = Math.random() * 2;
         this.speedX = (Math.random() - 0.5) * 0.5;
         this.speedY = (Math.random() - 0.5) * 0.5;
@@ -42,10 +42,10 @@ export function HeroSection() {
         this.x += this.speedX;
         this.y += this.speedY;
 
-        if (this.x > canvas.width) this.x = 0;
-        if (this.x < 0) this.x = canvas.width;
-        if (this.y > canvas.height) this.y = 0;
-        if (this.y < 0) this.y = canvas.height;
+        if (this.x > (canvas?.width || window.innerWidth)) this.x = 0;
+        if (this.x < 0) this.x = canvas?.width || window.innerWidth;
+        if (this.y > (canvas?.height || window.innerHeight)) this.y = 0;
+        if (this.y < 0) this.y = canvas?.height || window.innerHeight;
       }
 
       draw() {
